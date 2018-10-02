a891af5e1b6062c7bd1824f997ba9f4d0fa3b388
Enes KURU <enes793k@icloud.com>
2018-10-02 11:29:28 +0300

    pass param to page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+const News = ({match}) => {
+  return (<h1>News Page {match.params.id}</h1>)
+};
 
 class App extends Component {
   render() {
@@ -8,8 +12,13 @@ class App extends Component {
       <div className="App">
         <Router>
           <div>
-            <Route path="/" exact strict render={()=>{return (<h1>Home Page</h1>)}}/>{/*exact eşitse yönlendir demek, eğer konulmazsa / ve /contactda da '/' olduğu için homepage hep render edilir*/}
-            <Route path="/contact" exact strict render={()=>{return (<h1>Contact Page</h1>)}}/>{/*strict /contact ve /contact/ yerine sadece /contact ın geçerli olmasını sağlar, birebir eşirse render et demektir*/}
+            <Route path="/" exact strict render={() => {
+              return (<h1>Home Page</h1>)
+            }}/>{/*exact eşitse yönlendir demek, eğer konulmazsa / ve /contactda da '/' olduğu için homepage hep render edilir*/}
+            <Route path="/contact" exact strict render={() => {
+              return (<h1>Contact Page</h1>)
+            }}/>{/*strict /contact ve /contact/ yerine sadece /contact ın geçerli olmasını sağlar, birebir eşirse render et demektir*/}
+            <Route path="/news/:id" exact strict component={News}/>
           </div>
         </Router>
       </div>
